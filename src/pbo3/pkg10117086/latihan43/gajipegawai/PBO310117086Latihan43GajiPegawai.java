08c188dfd4ffdc64be47bda4ecab0e776128e2e6
djulfikar <muhammaddjulfikar7@gmail.com>
2018-10-21 16:21:48 +0700

    PBO3-10117086-Latihan43-GajiPegawai

---
+++ src/pbo3/pkg10117086/latihan43/gajipegawai/PBO310117086Latihan43GajiPegawai.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117086.latihan43.gajipegawai;
+
+/**
+ *
+ * @author Lenovo 
+ * NAMA : RD Muhammad Djulfikar BU 
+ * KELAS : IF3 
+ * NIM : 10117086
+ * DESKRIPSI PROGRAM :
+ */
+public class PBO310117086Latihan43GajiPegawai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GajiPegawai objG = new GajiPegawai();
+        objG.tampilData("", "", 0, 0, 0, 0);
+
+    }
+
+}
